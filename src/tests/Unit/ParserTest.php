c7477c9a74ba4d61da1069707e93ecffc2599b89
Vagif Rufullazada <vaqifrzade@gmail.com>
2021-02-03 22:19:02 +0400

    Created test case

--- src/tests/Unit/ParserTest.php
+++ src/tests/Unit/ParserTest.php
@@ -2,12 +2,23 @@
 
 namespace App\Tests\Unit;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
 
 class ParserTest extends TestCase
 {
     public function testParserStability()
     {
+        /*$mock = new MockHandler([
+            new Response(200, [], '')
+        ]);
+
+        $handler = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handler]);*/
+
         $this->assertTrue(true);
     }
 }
